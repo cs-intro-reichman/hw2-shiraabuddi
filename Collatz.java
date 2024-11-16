e6c6d9cf888eb28268f73b6b3cb8f57d74209786
Omer Abuddi <shira.abuddi@post.runi.ac.il>
2024-11-16 10:35:57 +0200

    test

--- Collatz.java
+++ Collatz.java
@@ -7,6 +7,7 @@ public class Collatz {
 		int numnow =0;
 		int index = 0;
 		if (mode.equals("v")){
+			System.err.println("");
 			for (int i = 0; i < numtime; i++) {
 				numnow = i + 1;
 				index = 2;
@@ -29,10 +30,14 @@ public class Collatz {
 					index++;
 				}
 				System.out.print("(" + index + ")");
+				System.err.println("");
 			
 			}
+			System.err.println("");
+			System.err.println("Every one of the first " + numtime +" hailstone sequences reached 1.");
 		}
 		if (mode.equals("c")){
+			System.err.println("");
 			System.err.println("Every one of the first " + numtime +" hailstone sequences reached 1.");
 		}
 
